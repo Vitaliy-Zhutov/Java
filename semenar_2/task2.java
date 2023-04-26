b97c9515c0e2108ef222652e003ab75b05a29df0
vitaly <vitaly.zutov@yandex.ru>
2023-04-26 23:57:59 +0700

    Сравнение скорости работы String и StringBulder

---
+++ semenar_2/task2.java
@@ -0,0 +1,31 @@
+package lessons_Java_GB.semenar_2;
+
+public class task2 {
+    public static void main(String[] args) {
+        
+        String myString = """
+                Самолет построим сами,
+                Понесемся над лесами.
+                Понесемся над лесами,
+                А потом вернемся к маме.""";
+        String query = "лесами";
+
+        double begin = System.currentTimeMillis();
+        System.out.println(myString);
+        System.out.println(myString.replace(query, "полями"));
+        double end = System.currentTimeMillis();
+        System.out.println("Время: " + (end - begin));
+
+        System.out.println();
+
+        begin = System.currentTimeMillis();
+        StringBuilder sb = new StringBuilder(myString);
+        while (sb.indexOf(query) > 0){
+            sb.replace(sb.indexOf(query), sb.indexOf(query) + query.length(), "полями");
+        }
+        System.out.println(sb);
+        end = System.currentTimeMillis();
+        System.out.println("Время: " + (end - begin));
+
+    }
+}
